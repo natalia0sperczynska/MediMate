bb1ffd456b264acffc3b60efb6264391571b608f
nsperczynska <278444@student.pwr.edu.pl>
2025-04-10 19:59:24 +0200

    firestore update

--- app/src/main/java/com/example/medimate/firebase/FireStoreDoctor.kt
+++ app/src/main/java/com/example/medimate/firebase/FireStoreDoctor.kt
@@ -87,7 +87,7 @@ class FireStoreDoctor {
         val result = mFireStore.collection("appointments").get().await()
         for (document in result) {
             val appointment = document.toObject(Appointment::class.java)
-            if (appointment.doctor.id == doctorId)
+            if (appointment.doctor?.id == doctorId)
                 appointmentsList.add(appointment)
         }
         return appointmentsList
