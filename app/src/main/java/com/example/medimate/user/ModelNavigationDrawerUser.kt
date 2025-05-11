cfc19aa5f738b23ab03e8563121e9e49ea12fe72
nsperczynska <sperczynskanatalia@gmail.com>
2025-05-11 15:06:56 +0200

    navigation drawer update

--- app/src/main/java/com/example/medimate/user/ModelNavigationDrawerUser.kt
+++ app/src/main/java/com/example/medimate/user/ModelNavigationDrawerUser.kt
@@ -1,14 +1,19 @@
 package com.example.medimate.user
+import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.DrawerState
@@ -21,17 +26,69 @@ import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.healme.R
 import com.example.medimate.navigation.Screen
 import com.example.medimate.ui.theme.Black
 import com.example.medimate.ui.theme.LightGrey
 import com.example.medimate.ui.theme.PurpleGrey
 import com.example.medimate.ui.theme.PurpleGrey2
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+@Composable
+fun DrawerHeader(){
+    val auth = FirebaseAuth.getInstance()
+    val name= auth.currentUser?.displayName
+    val email = auth.currentUser?.email
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .background(PurpleGrey.copy(alpha = 0.1f))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Box(
+            modifier = Modifier
+                .size(64.dp)
+                .background(LightGrey, CircleShape)
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.profile_pic),
+                contentDescription = "Profile",
+                modifier = Modifier.size(32.dp).align(Alignment.Center)
+            )
+        }
+        name?.let {
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = it,
+                style = MaterialTheme.typography.titleMedium,
+                color = Black
+            )
+        }
+
+        email?.let {
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(
+                text = it,
+                style = MaterialTheme.typography.bodySmall,
+                color = Black.copy(alpha = 0.7f)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+        HorizontalDivider(color = LightGrey)
+    }
+    }
 
 @Composable
 fun ModelNavDrawerUser(navController: NavController,
@@ -45,6 +102,7 @@ fun ModelNavDrawerUser(navController: NavController,
             ModalDrawerSheet(
                 modifier = Modifier.background(PurpleGrey2)
             ) {
+                DrawerHeader()
                 NavigationDrawerItem(
                     label = {Text(text="Menu", modifier = Modifier.padding(16.dp), color = Black)},
                     selected = false,
@@ -52,7 +110,7 @@ fun ModelNavDrawerUser(navController: NavController,
                 )
                 HorizontalDivider(color = LightGrey)
                 NavigationDrawerItem(
-                    label = { Text(text = "Appointments", color = Black) },
+                    label = { Text(text = "Set New Appointment", color = Black) },
                     selected = false,
                     onClick = { navController.navigate(Screen.Appointments.route) }
                 )
@@ -61,6 +119,17 @@ fun ModelNavDrawerUser(navController: NavController,
                     selected = false,
                     onClick = { navController.navigate(Screen.Doctors.route) }
                 )
+                NavigationDrawerItem(
+                    label = { Text(text = "My Appointments", color = Black) },
+                    selected = false,
+                    onClick = { navController.navigate(Screen.Doctors.route) }
+                )
+                NavigationDrawerItem(
+                    label = { Text(text = "Appointments History", color = Black) },
+                    selected = false,
+                    onClick = { navController.navigate(Screen.Doctors.route) }
+                )
+
             }
         }
     ) {
