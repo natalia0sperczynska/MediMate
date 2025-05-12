e5295ff348a159182edc50c73c1287c92c5a1789
nsperczynska <sperczynskanatalia@gmail.com>
2025-05-12 15:18:23 +0200

    email test sending working

--- app/src/main/java/com/example/medimate/mail/MailSender.kt
+++ app/src/main/java/com/example/medimate/mail/MailSender.kt
@@ -9,43 +9,30 @@ import javax.mail.*
 import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeMessage
 
-suspend fun sendMail(context: Context): Boolean = withContext(Dispatchers.IO) {
+suspend fun sendMail(context: Context, recipient: String): Boolean = withContext(Dispatchers.IO) {
     return@withContext try {
-        if (!NetworkUtils.isNetworkAvailable(context)) {
-            Log.e("EmailSender", "No network connection")
-            false
-        } else {
-            val props = Properties().apply {
-                put("mail.smtp.host", "live.smtp.mailtrap.io")
-                put("mail.smtp.port", "587")
-                put("mail.smtp.auth", "true")
-                put("mail.smtp.starttls.enable", "true")
-                put("mail.smtp.ssl.trust", "live.smtp.mailtrap.io")
-                put("mail.smtp.connectiontimeout", "10000")
-                put("mail.smtp.timeout", "10000")
-                put("mail.smtp.localhost", "android.local")
-            }
-
-            val session = Session.getInstance(props, object : Authenticator() {
-                override fun getPasswordAuthentication() =
-                    PasswordAuthentication("api", "378bea846f5253e27d48db583fae6604")
-            })
-
-            session.debug = true
-
-            val message = MimeMessage(session).apply {
-                setFrom(InternetAddress("medimate@demomailtrap.co"))
-                setRecipients(Message.RecipientType.TO, InternetAddress.parse("sperczynskanatalia@gmail.com"))
-                subject = "Test from MediMate"
-                setText("MediMate is sending greetings ! Register into our app please :(((((")
-                setHeader("Message-ID", "<${UUID.randomUUID()}@android.local>")
-            }
-
-            Transport.send(message)
-            true
+        val props = Properties().apply {
+            put("mail.smtp.host", "live.smtp.mailtrap.io") // Same host but now sends real emails
+            put("mail.smtp.port", "587")
+            put("mail.smtp.auth", "true")
+            put("mail.smtp.starttls.enable", "true")
         }
+
+        val session = Session.getInstance(props, object : Authenticator() {
+            override fun getPasswordAuthentication() =
+                PasswordAuthentication("api", "your_production_api_token") // Different token!
+        })
+
+        MimeMessage(session).apply {
+            setFrom(InternetAddress("noreply@yourdomain.com")) // Must be your verified domain
+            setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient))
+            subject = "Important message from MediMate"
+            setText("Hello from our app!")
+        }.let { Transport.send(it) }
+
+        true
     } catch (e: Exception) {
-        Log.e("EmailSender", "Error sending email", e)
+        Log.e("EmailSender", "Error sending real email", e)
         false
     }
 }
